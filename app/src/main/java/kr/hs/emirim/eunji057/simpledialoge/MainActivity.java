2e04f673ab4f00bb6f501480ab41f4b8750697d2
Eunji Na <neg219@naver.com>
2016-06-07 20:28:41 +0900

    #3 setPositiveButton to setEEvent Handler

--- app/src/main/java/kr/hs/emirim/eunji057/simpledialoge/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/eunji057/simpledialoge/MainActivity.java
@@ -1,13 +1,41 @@
 package kr.hs.emirim.eunji057.simpledialoge;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but=(Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("First Dialog");
+        dialog.setMessage("This is message part.");
+        dialog.setIcon(R.drawable.first_icon);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭했당!", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.show();
     }
 }
